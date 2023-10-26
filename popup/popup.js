2e5e43ea2012d99b511680938bd54d7a8f83222d
chauchakching <chauchakching@gmail.com>
2023-10-26 08:05:51 +0800

    add click time config

--- popup/popup.js
+++ popup/popup.js
@@ -7,18 +7,55 @@ if (typeof browser == "undefined") {
   globalThis.browser = chrome;
 }
 
-const checkbox_aggressive_pre_click_dom = document.getElementById(
-  "checkbox_aggressive_pre_click"
-);
-checkbox_aggressive_pre_click_dom.onchange = async () => {
-  await browser.storage.local.set({
-    checkbox_aggressive_pre_click: checkbox_aggressive_pre_click_dom.checked,
-  });
-};
+function getRadioGroupValue(name) {
+  return Array.from(document.querySelectorAll(`input[name=${name}]`)).find(
+    (dom) => dom.checked
+  )?.value;
+}
+function setRadioGroupValue(name, value) {
+  const dom = Array.from(document.querySelectorAll(`input[name=${name}]`)).find(
+    (dom) => dom.value === value
+  );
+  if (dom) {
+    console.log("set checked to true", dom);
+    dom.checked = true;
+  }
+}
 
 async function init() {
+  /**
+   * Register checkbox "checkbox_aggressive_pre_click"
+   */
+  const checkbox_aggressive_pre_click_dom = document.getElementById(
+    "checkbox_aggressive_pre_click"
+  );
   checkbox_aggressive_pre_click_dom.checked = (
     await browser.storage.local.get("checkbox_aggressive_pre_click")
   ).checkbox_aggressive_pre_click;
+  checkbox_aggressive_pre_click_dom.onchange = async () => {
+    await browser.storage.local.set({
+      checkbox_aggressive_pre_click: checkbox_aggressive_pre_click_dom.checked,
+    });
+  };
+
+  /**
+   * Register radio "countdown_type"
+   */
+  const countdown_type_doms = document.querySelectorAll(
+    "[name=countdown_type]"
+  );
+  setRadioGroupValue(
+    "countdown_type",
+    (await browser.storage.local.get("radio_countdown_type"))
+      .radio_countdown_type
+  );
+  Array.from(countdown_type_doms).forEach((dom) => {
+    dom.onchange = async () => {
+      console.log("radio onchange()");
+      await browser.storage.local.set({
+        radio_countdown_type: getRadioGroupValue("countdown_type"),
+      });
+    };
+  });
 }
 init();
